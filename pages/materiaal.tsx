a368689dc143170ed565048edfbd74f959f4f1ac
martijn van de ree <mrtjnvanderee@gmail.com>
2021-05-20 16:11:20 +0200

    -t

--- pages/materiaal.tsx
+++ pages/materiaal.tsx
@@ -22,7 +22,7 @@ const Materiaal: FunctionComponent<HomeProps> = ({ videos }) => {
 
           <div className="mb-8 ">
             <p className="text-lg border-b-2 border-gray-200 pb-2 text-fontColor pb-4 mb-4">
-              tVia onderstaande links kun je kijken tussen de werkvormen en
+              Via onderstaande links kun je kijken tussen de werkvormen en
               materialen die ik gemaakt heb voor mijn leerlingen. Gesorteerd op
               jaarlaag vind je werkvormen en presentaties voor economie lessen en in
               de map algemeen materialen die ook voor andere vakken toepasbaar zijn.
