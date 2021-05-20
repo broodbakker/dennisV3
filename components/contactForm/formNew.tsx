0d6b713b75d1afb5b528b42219f2bef1706c19b4
martijn van de ree <mrtjnvanderee@gmail.com>
2021-05-20 09:15:24 +0200

    changes to from

--- components/contactForm/formNew.tsx
+++ components/contactForm/formNew.tsx
@@ -9,25 +9,26 @@ export const ContactForm = () => {
 
 
       <div className="flex flex-wrap -mx-3 mb-2">
+
         <div className="w-full md:w-1/3 px-3 mb-2">
-          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-city">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="name">
             Wat is je naam?
       </label>
-          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Mijn naam" />
+          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="name" type="text" placeholder="Naam" />
         </div>
 
         <div className="w-full md:w-1/3 px-3 mb-2">
-          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="instantie">
             Wat is de naam van je instantie?
       </label>
-          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="instantie" />
+          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="instantie" type="text" placeholder="instantie" />
         </div>
 
         <div className="w-full md:w-1/3 px-3 mb-6 mb-2">
-          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-zip">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="email">
             Email
       </label>
-          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="email" placeholder="Vul hier je e-mailadres in" />
+          <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="Vul hier je e-mailadres in" />
         </div>
       </div>
 
@@ -38,15 +39,14 @@ export const ContactForm = () => {
         <div className="mt-1">
           <textarea id="about" name="about" rows={5} className="shadow-sm focus:ring-color3 focus:border-color3 mt-1 block w-full sm:text-sm border-gray-300 rounded-md border" placeholder="       Vul in..."></textarea>
         </div>
-        <p className="mt-2 text-sm text-gray-500">
 
-        </p>
       </div>
 
 
       <button className="bg-color3 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded mt-4 items-center" type="submit">
         Verzend
 </button>
+
     </form>
   )
 }
--- components/title.tsx
+++ components/title.tsx
@@ -15,7 +15,7 @@ export const Title: React.FunctionComponent<Props> = ({
     color={color}
     className={`text-center mb-8 my-auto pt-48 z-10 text-${color} `}
   >
-    <h1 className="text-3xl tracking-wider m-0 leading-normal md:text-4xl mb-1 ">
+    <h1 className="text-3xl tracking-wider m-0 leading-normal md:text-4xl mb-1 font-semibold">
       {title}
     </h1>
     <div className={`border-solid border border-color1 text-${color}`}></div>
