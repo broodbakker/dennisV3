00a0082cedf751caba7ea193bf2ce4f845a4dbe4
martijn van de ree <mrtjnvanderee@gmail.com>
2021-06-17 20:07:26 +0200

    added meta

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,8 +2,6 @@
 import React, { FunctionComponent } from 'react'
 import Head from 'next/head';
 
-
-
 type LayoutProps = {
   title: string
 }
@@ -13,6 +11,10 @@ export const Layout: FunctionComponent<LayoutProps> = ({ title, children }) => {
     <>
       <Head>
         <title>{title}</title>
+        <meta property="og:description" content="Dennis Stassen Enthousiast Econoom & Leergierige Leraar Ben je ook benieuwd welke lessen jij kunt trekken uit de praktijk van een economiedocent?
+        Hier deel ik mijn ideeën over goed lesgeven en mijn passie voor economie.
+Ik ben ervan overtuigd dat veel inzichten vanuit het klaslokaal toepasbaar in alle situaties waar je mensen wil boeien, enthousiasmeren of overtuigen!" />
+
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
       {children}
